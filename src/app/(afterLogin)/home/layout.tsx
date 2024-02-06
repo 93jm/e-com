497b4e88f64c5595b441486c0bcc388af27b0fcb
Evan <93_jm@naver.com>
2024-02-06 19:57:22 +0900

    intial setting

---
+++ src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomeLayout({children}: any) {
+    return (
+        <div>
+            홈 레이아웃1
+            {children}
+        </div>
+    );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <p>찾는 페이지가 없습니다 ㅋㅋ</p>
+    </div>
+  );
+}
+
+export default NotFound;
